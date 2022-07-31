3d5cf86c616be392be75586a099dc97e99fa58bf
Kateryna Magur <64228839+KaterynaMagur@users.noreply.github.com>
2022-07-31 21:19:20 +0300

    added layouts and router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import {CssBaseline} from "@mui/material";
+import {Route, Routes, Navigate} from "react-router-dom";
+import {Dashboard} from "./layouts/Dashboard/Dashboard";
+import {Goals} from "./layouts/Goals/Goals";
+import {MainLayout} from "./layouts/MainLayout/MainLayout";
 
 
 function App() {
   return (
-   <div>
-     <CssBaseline />
-   </div>
+    <MainLayout>
+      <Routes>
+        <Route index path="dashboard" element={<Dashboard />} />
+        <Route path="goals" element={<Goals />} />
+        <Route path="*" element={<Navigate to="dashboard" replace />} />
+      </Routes>
+    </MainLayout>
   );
 }
 
