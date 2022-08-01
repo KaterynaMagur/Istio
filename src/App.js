67d02fb93dbb2e10a7e83e93cced60bfc7a8378b
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-08-01 16:20:35 +0300

    take all from gen repo and fixing bags

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
 import {Route, Routes, Navigate} from "react-router-dom";
-import {Dashboard} from "./layouts/Dashboard/Dashboard";
-import {Goals} from "./layouts/Goals/Goals";
-import {MainLayout} from "./layouts/MainLayout/MainLayout";
+
+import './App.css';
+import {MainLayout} from "./Layouts/MainLayout/MainLayout.js";
+import {Dashboard} from "./Layouts/Dashboard/Dashboard";
+import {Goals} from "./Layouts/Goals/Goals";
 
 
 function App() {
-  return (
-    <MainLayout>
-      <Routes>
-        <Route index path="dashboard" element={<Dashboard />} />
-        <Route path="goals" element={<Goals />} />
-        <Route path="*" element={<Navigate to="dashboard" replace />} />
-      </Routes>
-    </MainLayout>
-  );
+    return (
+        <MainLayout>
+            <Routes>
+                <Route index path="dashboard" element={<Dashboard/>}/>
+                <Route path="goals" element={<Goals/>}/>
+                <Route path="*" element={<Navigate to="dashboard" replace/>}/>
+            </Routes>
+        </MainLayout>
+    );
 }
 
 export default App;
