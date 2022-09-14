e167dee8e8409c3a5f18c97db8b9e911c878d21c
YuriyVoitykhivskyi <gvoit801@gmail.com>
2022-09-14 16:33:34 +0300

    adding google logination to form component

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -29,6 +29,11 @@ export default function LoginForm() {
         reset();
     }
 
+    const loginWithGoogle = () => {
+      setAuthMode(authMode === "signin" ? "loginwithGoogle" : "signin");
+      reset();
+  }
+
     //react-hook-form settings
     const {
         register,
@@ -131,6 +136,11 @@ export default function LoginForm() {
                   <Grid item xs>
                     <Link href="#" variant="body2" onClick={changeLoginToResetPass} >
                       Забули пароль ?
+                    </Link>
+                  </Grid>
+                  <Grid item xs>
+                    <Link href="#" variant="body2" onClick={loginWithGoogle} >
+                      Google логінація 
                     </Link>
                   </Grid>
                   <Grid item>
@@ -329,4 +339,64 @@ export default function LoginForm() {
             
         );
     }   
+
+
+
+    if (authMode === "loginwithGoogle") {
+      return (
+          <Container component="main"  sx={{
+              mt: 1, mb: 1 , 
+              float: 'right',
+              width: '300px',
+              padding: '0 !important',
+              zIndex: 1
+          }} >
+          <CssBaseline />
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              position: 'relative',
+              marginTop: 7,
+              bgcolor: '#e3f2fd',
+              padding: '10px 20px',
+              borderRadius: '20px',
+              boxShadow: `0px 10px 13px -6px rgb(0 0 0 / 20%),
+                          0px 20px 31px 3px rgb(0 0 0 / 14%),
+                          0px 8px 38px 7px rgb(0 0 0 / 12%)`,
+              zIndex: 1,
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5" >
+              Google логінація
+            </Typography>
+  
+            <Box component="form" onSubmit={handleSubmit(onSubmit)}  sx={{ mt: 1 }}>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                onClick={ () =>  }
+
+                sx={{ mt: 3, mb: 2 }}
+              >
+                  Увійти з допомогою Google акаунту
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <Link href="#" onClick={changeAuthMode} variant="body2">
+                    {"Повернутися на сторінку входу"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+          
+      )
+  };
 }
