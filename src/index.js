4079f03cd8bb85cdcf3dbd3ed0a51eec34fd58f4
Kateryna Magur <64228839+KaterynaMagur@users.noreply.github.com>
2022-09-29 20:23:49 +0300

    added goals popup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,16 +8,19 @@ import {theme} from "./theme";
 import {BrowserRouter} from "react-router-dom";
 import {CssBaseline} from "@mui/material";
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <CssBaseline />
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>
+
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <CssBaseline />
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
